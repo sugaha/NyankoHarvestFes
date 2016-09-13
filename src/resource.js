ff697bee293dc8dee10d5759a89ee89a0b48a5df
sugaha <gorba1442@gmail.com>
2016-09-13 13:51:45 +0900

    画像差し替え

---
+++ src/resource.js
@@ -0,0 +1,15 @@
+var res = {
+  bomb_png: "res/s/bug.png",
+  cat_png: "res/s/cat0.png",
+  apple_png: "res/s/apple.png",
+//  leftbutton_png: "res/leftbutton.png",
+//  rightbutton_png: "res/rightbutton.png",
+  background_png: "res/s/game_bg.png",
+//  touchorigin_png: "res/touchorigin.png",
+//  touchend_png: "res/touchend.png"
+};
+
+var g_resources = [];
+for (var i in res) {
+  g_resources.push(res[i]);
+}
